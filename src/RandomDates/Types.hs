1a3be0750ce2b6b691ca941a9c13f272507b16bf
Eric Rochester <erochest@virginia.edu>
2014-10-23 16:23:34 -0400

    DateRow

--- src/RandomDates/Types.hs
+++ src/RandomDates/Types.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
 
@@ -7,11 +8,22 @@ module RandomDates.Types
     , optN
     , optCenterDate
     , optStandardDev
+
+    , DateRow
+    , rowTitle
+    , rowDescr
+    , rowDate
     ) where
 
 
 import           Control.Lens
+import qualified Data.ByteString.Char8       as B
+import           Data.CSV.Conduit.Conversion
+import qualified Data.Map.Strict             as M
+import qualified Data.Text                   as T
+import           Data.Text.Encoding
 import           Data.Thyme
+import           System.Locale
 
 
 data RandomOpts = RandomOpts
@@ -20,3 +32,19 @@ data RandomOpts = RandomOpts
                 , _optStandardDev :: !Int
                 } deriving (Show)
 makeLenses ''RandomOpts
+
+data DateRow = DateRow
+             { _rowTitle :: !T.Text
+             , _rowDescr :: !T.Text
+             , _rowDate  :: !Day
+             } deriving (Show)
+makeLenses ''DateRow
+
+instance ToNamedRecord DateRow where
+    toNamedRecord DateRow{..} =
+        M.fromList [ ("title"       , encodeUtf8 _rowTitle)
+                   , ("description" , encodeUtf8 _rowDescr)
+                   , ("date"        , format _rowDate)
+                   ]
+        where
+            format = B.pack . formatTime defaultTimeLocale "%Y-%m-%d"
